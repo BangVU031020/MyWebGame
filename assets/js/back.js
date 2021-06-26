9136d272d56914758030ca6deac6c73840bac7df
bn <bn@DESKTOP-P81EGPL>
2021-06-26 16:24:55 +0700

    Add project files.

---
+++ assets/js/back.js
@@ -0,0 +1,16 @@
+//xử button trở lại
+var btn = $('#button');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
+
